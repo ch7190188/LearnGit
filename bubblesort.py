f1c413675d50428d5d5319faedf10cc759f45dcf
Pushp Raj Singh <ch7190188@iitd.ac.in>
2020-05-12 22:42:09 +0530

    bubblesort is added to master

---
+++ bubblesort.py
@@ -0,0 +1,11 @@
+def bubblesort(L):
+	for i in range(1,len(L)):
+		for j in range(len(L)-i):
+			if L[j]>L[j+1]:
+				L[j],L[j+1]=L[j+1],L[j]
+a=[82,4,5,3,45,3,44]
+bubblesort(a)
+print(a)
+
+
+print("in Dev branch")
